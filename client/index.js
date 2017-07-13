c3e29f407296ec0b9f8b20011e5e96cdd832a00e
Ændrew Rininsland <andrew.rininsland@ft.com>
2017-07-13 17:46:21 +0100

    Initial

--- client/index.js
+++ client/index.js
@@ -1,19 +1,15 @@
-/*
-  TODO: delete this comment
+import * as ch6 from './chapter6/index';
+import * as ch7 from './chapter7/index';
 
-  This file is where you bootstrap your JS code
-  For example import stuff here:
+ch6.GoTChart('tree', 'data/GoT-lineage-by-father.json');
+ch6.GoTChart('cluster', 'data/GoT-lineage-by-father.json');
+ch6.GoTChart('treemap', 'data/GoT-lineages-screentimes.json');
+ch6.GoTChart('partition', 'data/GoT-lineages-screentimes.json');
+ch6.GoTChart('pack', 'data/GoT-lineages-screentimes.json');
 
-  import {select} from 'd3-selection';
-  import myComponent from './components/my-component';
-
-  Split logical parts of you project into components e.g.
-
-  /client
-    - /components
-        - /component-name
-            - styles.scss
-            - index.js
-            - template.html
-
-*/
+ch7.GoTChart('pie', 'data/GoT-lineages-screentimes.json');
+ch7.GoTChart('histogram', 'data/GoT-deaths-by-season.json');
+ch7.GoTChart('chord', 'data/stormofswords.csv');
+ch7.GoTChart('force', 'data/stormofswords.csv');
+ch7.GoTChart('stack', 'data/GoT-deaths-by-season.json', false);
+ch7.GoTChart('stack', 'data/GoT-deaths-by-season.json', true);
